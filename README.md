4bc27f74e97e0263c8ea71db9ce875bead38698c
Garrett Michael Flynn <garrettmflynn@gmail.com>
2025-02-07 18:07:28 -0800

    Fix for MacOS

--- README.md
+++ README.md
@@ -14,6 +14,9 @@
 - [ ] Volume
 - [ ] Mouse jitter (e.g. robot.js) 
 
+## Improvements
+- Create multiple windows for different screens in a multi-monitor setup
+
 ## Issues
 - Every once in a while, the brightness stops responding...
 - Robot.js [will not run on Electron](https://github.com/octalmage/robotjs/issues/675)
--- commoners.config.ts
+++ commoners.config.ts
@@ -95,14 +95,18 @@ export default {
             desktop: {
                 load: function (win) {
                     const mainScreen = this.electron.screen.getPrimaryDisplay()
+
                     win.setVisibleOnAllWorkspaces(true, { visibleOnFullScreen: true })
                     win.setAlwaysOnTop(true, 'screen-saver', 1)
-                    
-                    win.setIgnoreMouseEvents(true, { forward: true });
+                    win.setFullScreenable(false);
+                    win.setResizable(false); // Ensure over the dock on MacOS
+                    win.moveTop();
                     
                     win.setPosition(mainScreen.bounds.x, mainScreen.bounds.y)
                     win.setSize(mainScreen.bounds.width, mainScreen.bounds.height)
 
+                    // Ensure the window cannot be interacted with unless exceptions are specified
+                    win.setIgnoreMouseEvents(true, { forward: true });
                     this.on("set-ignore-mouse-events", (event, ignore) => win.setIgnoreMouseEvents(ignore, { forward: true }));
 
                     // Ensure you can always exit the app
